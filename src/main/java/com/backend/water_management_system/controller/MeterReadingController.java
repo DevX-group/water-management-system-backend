c8c0d108324a7f32a215e2fcadfd68eb4b6ee444
kaweesha weerasinghe <weerasinghamkh.23@uom.lk>
2026-02-18 20:46:37 +0530

    add controllers for bill calculation and meter reading

---
+++ src/main/java/com/backend/water_management_system/controller/MeterReadingController.java
@@ -0,0 +1,35 @@
+package com.backend.water_management_system.controller;
+
+import com.backend.water_management_system.dto.MeterReadingCreateRequest;
+import com.backend.water_management_system.dto.MeterReadingCreateResponse;
+import com.backend.water_management_system.entity.Bill;
+import com.backend.water_management_system.service.MeterReadingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/meter-readings")
+@CrossOrigin
+public class MeterReadingController {
+
+    private final MeterReadingService meterReadingService;
+
+    public MeterReadingController(MeterReadingService meterReadingService) {
+        this.meterReadingService = meterReadingService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MeterReadingCreateResponse> submit(@RequestBody MeterReadingCreateRequest req) {
+        Bill bill = meterReadingService.submitReadingAndGenerateBill(req);
+
+        MeterReadingCreateResponse res = new MeterReadingCreateResponse();
+        res.readingId = bill.getMeterReading().getReadingId();
+        res.usageUnits = bill.getUsageUnits();
+        res.billId = bill.getBillId();
+        res.totalAmount = bill.getTotalAmount();
+        res.status = bill.getStatus();
+
+        return ResponseEntity.ok(res);
+    }
+}
+
