15e3a19742a97056e6fb8b750982ddc96135c3a4
hansanaUOM <thilakarathnamhm.23@uom.lk>
2026-02-22 03:08:36 +0530

    fix security issue with messaging

--- src/main/java/com/backend/water_management_system/config/SecurityConfig.java
+++ src/main/java/com/backend/water_management_system/config/SecurityConfig.java
@@ -1,24 +1,5 @@
-// package com.backend.water_management_system.config;
+package com.backend.water_management_system.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.Customizer;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
+// Spring Security is currently disabled.
+// Dependency removed from pom.xml.
 
-// @Configuration
-// public class SecurityConfig{
-
-//     @Bean
-//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//         http
-//                 .csrf(csrf -> csrf.disable())
-//                 .authorizeHttpRequests(auth -> auth
-//                         .requestMatchers("/api/**").permitAll()
-//                         .anyRequest().authenticated()
-//                 )
-//                 .httpBasic(Customizer.withDefaults());
-
-//         return http.build();
-//     }
-// }
