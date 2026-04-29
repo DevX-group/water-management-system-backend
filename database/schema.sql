62af867ebb6ed43cd7877b3545328e87dc4b8621
Kave_en <kaveendinuka04@gmail.com>
2026-04-29 20:08:43 +0530

    Customer Report section

--- database/schema.sql
+++ database/schema.sql
@@ -10,3 +10,15 @@ select *
 from monthly_reports
 
 GRANT ALL PRIVILEGES ON TABLE monthly_reports TO hydropay_user;
+
+
+--monthly report query to replace later
+
+SELECT
+  TO_CHAR(record_date, 'Mon') AS month,
+  SUM(usage) AS usage,
+  SUM(amount) AS revenue
+FROM usage_records
+WHERE EXTRACT(YEAR FROM record_date) = 2026
+GROUP BY TO_CHAR(record_date, 'Mon'), EXTRACT(MONTH FROM record_date)
+ORDER BY EXTRACT(MONTH FROM record_date);
