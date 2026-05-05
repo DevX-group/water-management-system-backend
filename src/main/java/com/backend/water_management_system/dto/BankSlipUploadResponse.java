4e1b04bf38b03da98956daebb3151a9d31d991ee
dehemiparamitha <dehemiparamitha21@gmail.com>
2026-05-05 18:57:08 +0530

    refactor(dto): update existing DTOs and add new response DTO

--- src/main/java/com/backend/water_management_system/dto/BankSlipUploadResponse.java
+++ src/main/java/com/backend/water_management_system/dto/BankSlipUploadResponse.java
@@ -1,9 +1,9 @@
 package com.backend.water_management_system.dto;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import com.backend.water_management_system.entity.PaymentType;
 import com.backend.water_management_system.entity.SlipStatus;
 
 import lombok.AllArgsConstructor;
@@ -17,11 +17,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BankSlipUploadResponse {
     private String message;
-    private PaymentType paymentType;
+    private Long slipId;
     private BigDecimal amount;
     private String bankReference;
     private String filePath;
     private SlipStatus status;
     private LocalDateTime uploadedAt;
+    private LocalDate bankPaymentDate;
 
 }
