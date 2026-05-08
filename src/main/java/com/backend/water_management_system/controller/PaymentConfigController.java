9a8193593364299bc1649d48cf50e4ecfc6f259a
dehemiparamitha <dehemiparamitha21@gmail.com>
2026-05-08 19:21:47 +0530

    feat: add backend API for payment bank details

---
+++ src/main/java/com/backend/water_management_system/controller/PaymentConfigController.java
@@ -0,0 +1,27 @@
+package com.backend.water_management_system.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.water_management_system.dto.BankDetailsResponse;
+import com.backend.water_management_system.service.PaymentConfigService;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+@RequestMapping("/api/public/payments")
+public class PaymentConfigController {
+    private final PaymentConfigService paymentConfigService;
+
+    public PaymentConfigController(PaymentConfigService paymentConfigService) {
+        this.paymentConfigService = paymentConfigService;
+    }
+
+    @GetMapping("/bank-details")
+    public ResponseEntity<BankDetailsResponse> getBankDetails() {
+        return ResponseEntity.ok(paymentConfigService.getBankDetails());
+    }
+}
+
