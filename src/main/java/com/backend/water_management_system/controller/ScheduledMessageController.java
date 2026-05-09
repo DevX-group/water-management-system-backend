85eba42ff2291279cc8790165e8aded7d9ed412e
hansanaUOM <thilakarathnamhm.23@uom.lk>
2026-05-09 23:09:38 +0530

    feat(triggered-messages): create TriggeredMessage Entity and make CRUD operations

--- src/main/java/com/backend/water_management_system/controller/ScheduledMessageController.java
+++ src/main/java/com/backend/water_management_system/controller/ScheduledMessageController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/messages")
+@RequestMapping("/api/scheduled-messages")
 @CrossOrigin
 @RequiredArgsConstructor
 public class ScheduledMessageController {
---
+++ src/main/java/com/backend/water_management_system/entity/TriggeredMessage.java
@@ -0,0 +1,25 @@
+package com.backend.water_management_system.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "triggered_messages")
+@NoArgsConstructor
+@Getter
+@Setter
+public class TriggeredMessage extends Message {
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "trigger_type")
+    private TriggerType triggerType;
+
+    @Column(name = "active")
+    private boolean active;
+}
