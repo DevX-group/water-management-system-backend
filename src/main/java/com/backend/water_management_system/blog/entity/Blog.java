22805a2476454d79281c69483488321df8fe000e
hansanaUOM <thilakarathnamhm.23@uom.lk>
2026-05-10 19:10:54 +0530

    refactor: create blog package

--- src/main/java/com/backend/water_management_system/blog/entity/Blog.java
+++ src/main/java/com/backend/water_management_system/blog/entity/Blog.java
@@ -1,4 +1,4 @@
-package com.backend.water_management_system.billing.entity;
+package com.backend.water_management_system.blog.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
--- src/main/java/com/backend/water_management_system/blog/repository/BlogRepository.java
+++ src/main/java/com/backend/water_management_system/blog/repository/BlogRepository.java
@@ -1,9 +1,9 @@
-package com.backend.water_management_system.billing.repository;
+package com.backend.water_management_system.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.backend.water_management_system.billing.entity.Blog;
+import com.backend.water_management_system.blog.entity.Blog;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, Long> {
