03bf5ff4bf3abb92594c327974caa1c3afdcc2ad
hansanaUOM <thilakarathnamhm.23@uom.lk>
2026-05-17 19:34:24 +0530

    feat(payment-confirmation-message): send confirmation for online payments

--- src/main/java/com/backend/water_management_system/messaging/service/SentMessageService.java
+++ src/main/java/com/backend/water_management_system/messaging/service/SentMessageService.java
@@ -13,8 +13,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class SentMessageService {
