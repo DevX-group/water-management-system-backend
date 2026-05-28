4c48f4d09e3c98a902af5ed2b682a5e2c7e15ff4
hansanaUOM <thilakarathnamhm.23@uom.lk>
2026-05-28 10:01:48 +0530

    refactor(messaging): create a separate dto for MessagingEnumsResponse

---
+++ src/main/java/com/backend/water_management_system/messaging/dto/MessagingEnumsResponse.java
@@ -0,0 +1,15 @@
+package com.backend.water_management_system.messaging.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MessagingEnumsResponse {
+    
+    private final List<String> channels;
+    private final List<String> scheduleTypes;
+    private final List<String> recipientTypes;
+}
